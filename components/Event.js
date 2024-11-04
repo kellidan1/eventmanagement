e54268b378e4b8e2a252bfbdd16ca4b76222018d
Kellidan <21891.kellidan.sdcce@vvm.edu.in>
2024-11-04 17:06:26 +0530

    image display

--- components/Event.js
+++ components/Event.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, TextInput, TouchableOpacity, Button, Image } from 'react-native';
-import SubEvent from '../components/SubEvent';
 import { useNavigation } from '@react-navigation/native';
 import { Picker } from '@react-native-picker/picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as ImagePicker from 'expo-image-picker';
-const imageSource = require('../assets/images/icon.png'); // change it so user has option to click and choose their own image
+
+const defaultImageSource = require('../assets/images/icon.png');
 
 export default function Event() {
     const navigation = useNavigation();
@@ -20,15 +20,19 @@ export default function Event() {
         location: '',
         capacity: 0
     });
-
     const [image, setImage] = useState(null);
-
     const [venueList, setVenueList] = useState([]);
     const [showStartTime, setShowStartTime] = useState(false);
     const [showEndTime, setShowEndTime] = useState(false);
 
     const pickImage = async () => {
-        // No permissions request is necessary for launching the image library
+        const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+        if (permissionResult.granted === false) {
+            alert("Permission to access camera roll is required!");
+            return;
+        }
+
         let result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ImagePicker.MediaTypeOptions.All,
             allowsEditing: true,
@@ -36,40 +40,32 @@ export default function Event() {
             quality: 1,
         });
 
-        console.log(result);
-
         if (!result.canceled) {
-            setImage(result.assets[0].uri);
+            setImage(result.assets[0].uri); // Update the state with the chosen image URI
         }
     };
 
-    // load the venues
     useEffect(() => {
         fetch("https://event-management-backend-974j.onrender.com/venues")
             .then((res) => res.json())
             .then((venues) => {
-                let temp = []
-                temp.push({ id: "000", name: "Select Venue" }) // empty item
+                let temp = [];
+                temp.push({ id: "000", name: "Select Venue" });
                 venues.forEach((venue) => {
-                    temp.push({ id: venue._id, name: venue.name, location: venue.location, capacity: venue.capacity })
-                })
-                console.log(temp)
+                    temp.push({ id: venue._id, name: venue.name, location: venue.location, capacity: venue.capacity });
+                });
                 setVenueList(temp);
             })
-            .catch(err => { console.error(err) })
-    }, [])
-
-
+            .catch(err => console.error(err));
+    }, []);
 
     const handleCreateEvent = () => {
-        console.log('Creating event:', name, description, startTime, endTime); //replace this with inserting into database logic
-        navigation.navigate('Event Management App')
-
+        console.log('Creating event:', name, description, startTime, endTime);
 
         fetch("https://event-management-backend-974j.onrender.com/event", {
             method: "POST",
             headers: {
-                "Content-Type": "application/json", // Specify the content type
+                "Content-Type": "application/json",
             },
             body: JSON.stringify({
                 name,
@@ -86,64 +82,53 @@ export default function Event() {
                 subEvent
             })
         })
-            .then(res => { console.log(res); navigation.navigate('Event Management App') })
-            .catch(err => console.error(err))
+        .then(res => {
+            console.log(res);
+            navigation.navigate('Event Management App');
+        })
+        .catch(err => console.error(err));
     };
 
     return (
         <View style={styles.event}>
-
-            <TouchableOpacity onPress={pickImage}>
-                <Image source={imageSource} style={styles.image} />
+            <TouchableOpacity onPress={pickImage} style={styles.imageContainer}>
+                {image ? (
+                    <Image source={{ uri: image }} style={styles.image} />
+                ) : (
+                    <Image source={defaultImageSource} style={styles.image} />
+                )}
             </TouchableOpacity>
-            {/* <Button title="Pick an image from camera roll" onPress={pickImage} /> */}
-            {image && <Image source={{ uri: image }} />}
-            <TextInput
-                style={styles.input}
-                placeholder="Event Name"
-                value={name}
-                onChangeText={setName}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Event Description"
-                value={description}
-                onChangeText={setDescription}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Sub Event Name"
-                value={subEvent}
-                onChangeText={setSubEvent}
-            />
+            <TextInput style={styles.input} placeholder="Event Name" value={name} onChangeText={setName} />
+            <TextInput style={styles.input} placeholder="Event Description" value={description} onChangeText={setDescription} />
+            <TextInput style={styles.input} placeholder="Sub Event Name" value={subEvent} onChangeText={setSubEvent} />
             <View style={styles.button}>
-                <Button title={"Start Time: " + startTime.toLocaleTimeString()} onPress={() => setShowStartTime(true)} style={styles.button} />
+                <Button title={"Start Time: " + startTime.toLocaleTimeString()} onPress={() => setShowStartTime(true)} />
             </View>
             <View style={styles.button}>
                 <Button title={"End Time: " + endTime.toLocaleTimeString()} onPress={() => setShowEndTime(true)} />
             </View>
-
-
-            {showStartTime && (<DateTimePicker
-                value={new Date()}
-                mode={'time'}
-                is24Hour={true}
-                onChange={(_, selectedDate) => {
-                    setShowStartTime(false);
-                    setStartTime(selectedDate)
-                }}
-            />)}
-
-            {showEndTime && (<DateTimePicker
-                value={new Date()}
-                mode={'time'}
-                is24Hour={true}
-                onChange={(_, selectedDate) => {
-                    setShowEndTime(false);
-                    setEndTime(selectedDate)
-                }}
-            />)}
-
+            {showStartTime && (
+                <DateTimePicker
+                    value={startTime}
+                    mode='time'
+                    is24Hour={true}
+                    onChange={(_, selectedDate) => {
+                        setShowStartTime(false);
+                        setStartTime(selectedDate);
+                    }}
+                />
+            )}
+            {showEndTime && (
+                <DateTimePicker
+                    value={endTime}
+                    mode='time'
+                    is24Hour={true}
+                    onChange={(_, selectedDate) => {
+                        setShowEndTime(false);
+                        setEndTime(selectedDate);
+                    }}
+                />
+            )}
             <Picker
                 style={styles.picker}
                 selectedValue={selectedVenue.id}
@@ -154,7 +139,6 @@ export default function Event() {
                 ))}
             </Picker>
             <Button title="Create Event" onPress={handleCreateEvent} />
-
         </View>
     );
 }
@@ -166,23 +150,19 @@ const styles = StyleSheet.create({
         padding: 16,
         margin: 10,
     },
-    eventContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 16,
-        alignSelf: 'center',
-    },
-    image: {
+    imageContainer: {
         width: 200,
         height: 200,
         borderRadius: 10,
         marginBottom: 16,
         alignSelf: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        borderRadius: 10,
     },
     input: {
         width: '100%',
